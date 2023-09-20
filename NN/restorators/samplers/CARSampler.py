fix masking issue

NN/restorators/samplers/CARSampler.py
@@ -82,8 +82,7 @@ class CARSamplingAlgorithm(ISamplingAlgorithm):
       **state
     )
   
-  # remove 'iteration' from the kwargs to avoid conflicts with the 'iteration' argument in other methods
-  def firstStep(self, value, iteration, **kwargs):
+  def firstStep(self, value, **kwargs):
     B = tf.shape(value)[0]
     mask = tf.fill((B, ), True)
     # update kwargs, replace/store some data/callbacks

NN/restorators/samplers/CBasicInterpolantSampler.py
@@ -18,13 +18,12 @@ class CBasicInterpolantSampler:
     algorithm = kwargs.get('algorithmInterceptor', lambda x: x)( self._algorithm )
     assert isinstance(algorithm, ISamplingAlgorithm), f'Algorithm must be an instance of ISamplingAlgorithm, but got {type(algorithm)}'
 
-    # perform sampling
-    step = algorithm.firstStep(value=value, iteration=0, **kwargs)
+    step = algorithm.firstStep(value=value, **kwargs)
     # CFakeObject is a namedtuple, so we need to check for it
     if isinstance(step, tuple) and not is_namedtuple(step):
       step, kwargs = step # store some data in kwargs, because TF a bit stupid
 
-    iteration = tf.constant(1, dtype=tf.int32) # first step is already done
+    iteration = tf.constant(0, dtype=tf.int32)
     while tf.reduce_any(step.active):
       KWArgs = dict(
         value=value, step=step, iteration=iteration,

NN/restorators/samplers/CSamplerWatcher.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from .ISamplingAlgorithm import ISamplingAlgorithm
+from NN.utils import is_namedtuple
 
 class CSamplingInterceptor(ISamplingAlgorithm):
   def __init__(self, watcher, algorithm):
@@ -9,45 +10,38 @@ class CSamplingInterceptor(ISamplingAlgorithm):
   
   def firstStep(self, **kwargs):
     res = self._algorithm.firstStep(**kwargs)
-    self._watcher._onStep(
-      iteration=kwargs['iteration'],
-      value=kwargs['value'],
-      kwargs=kwargs
-    )
+    self._watcher._onStart(value=kwargs['value'], kwargs=kwargs)
     return res
   
   def nextStep(self, **kwargs):
+    self._watcher._onNextStep(iteration=kwargs['iteration'], kwargs=kwargs)
     res = self._algorithm.nextStep(**kwargs)
-    self._watcher._onStep(
-      iteration=kwargs['iteration'],
-      value=kwargs['solution'].value,
-      kwargs=kwargs
-    )
     return res
   
   def inference(self, **kwargs):
-    res = self._algorithm.inference(**kwargs)
-    return res
+    return self._algorithm.inference(**kwargs)
   
   def solve(self, **kwargs):
-    res = self._algorithm.solve(**kwargs)
-    self._watcher._onSolve(res, kwargs)
-    return res
+    return self._algorithm.solve(**kwargs)
 # End of CSamplingInterceptor
 
 class CSamplerWatcher:
   def __init__(self, steps, tracked, indices=None):
-    self._steps = steps
     self._indices = tf.constant(indices, dtype=tf.int32) if not(indices is None) else None
     self._tracked = {}
-    prefix = [steps + 1]
+    prefix = [steps]
     if not(self._indices is None):
-      prefix = [steps + 1, tf.size(self._indices)]
+      prefix = [steps, tf.size(self._indices)]
 
     for name, shape in tracked.items():
       shp = prefix + list(shape)
       self._tracked[name] = tf.Variable(tf.zeros(shp), trainable=False)
       continue
+
+    if 'value' in self._tracked: # value has steps + 1 shape, so we need extra variable
+      shp = prefix + list(tracked['value'])
+      self._initialValue = tf.Variable(tf.zeros(shp[1:]), trainable=False)
+      pass
 
     self._iteration = tf.Variable(0, trainable=False)
     return
@@ -60,29 +54,53 @@ class CSamplerWatcher:
     return lambda algorithm: CSamplingInterceptor(watcher=self, algorithm=algorithm)
   
   def tracked(self, name):
-    return self._tracked.get(name, None)
+    res = self._tracked.get(name, None)
+    if res is None: return None
+    if 'value' == name: # add initial value
+      res = tf.concat([self._initialValue[None], res], axis=0)
+    return res
   
-  def _updateTracked(self, name, value, iteration):
+  def _updateTracked(self, name, value, mask=None):
     tracked = self._tracked.get(name, None)
     if tracked is None: return
-    if not(self._indices is None):
-      value = tf.gather(value, self._indices, axis=0)
+    value = self._withIndices(value)
+    
+    iteration = self._iteration
+    if (mask is None) or ('value' == name): # 'value' is always unmasked
+      tracked[iteration].assign(value)
+      return
+    
+    mask = self._withIndices(mask)
+    tf.assert_equal(tf.shape(mask)[:1], tf.shape(value)[:1], 'Must have the same batch size')
+    prev = tracked[iteration - 1]
+    # expand mask to match the value shape by copying values from the previous iteration
+    indices = tf.where(mask)
+    value = tf.tensor_scatter_nd_update(prev, indices, value)
+    tf.assert_equal(tf.shape(prev), tf.shape(value), 'Must be the same shape')
     tracked[iteration].assign(value)
     return
   
-  def _onStep(self, iteration, value, kwargs):
-    self._updateTracked('value', value, iteration=iteration)
-
+  def _onNextStep(self, iteration, kwargs):
     self._iteration.assign(iteration)
-    return
-  
-  def _onSolve(self, solution, kwargs):
-    assert isinstance(solution, tuple), 'Must be a tuple'
-    assert hasattr(solution, '_fields'), 'Must be a namedtuple'
-    iteration = kwargs['iteration']
+    # track also solution
+    solution = kwargs['solution']
+    assert is_namedtuple(solution), 'Solution must be a namedtuple'
+    step = kwargs['step']
+    assert is_namedtuple(step), 'Step must be a namedtuple'
+    mask = step.mask if hasattr(step, 'mask') else None
     # iterate over all fields
     for name in solution._fields:
-      self._updateTracked(name, getattr(solution, name), iteration=iteration)
+      self._updateTracked(name, getattr(solution, name), mask=mask)
       continue
     return
+  
+  def _onStart(self, value, kwargs):
+    self._iteration.assign(0)
+    if 'value' in self._tracked: # save initial value
+      self._initialValue.assign( self._withIndices(value) )
+    return
+  
+  def _withIndices(self, value):
+    if self._indices is None: return value
+    return tf.gather(value, self._indices, axis=0)
 # End of CSamplerWatcher

tests/test_CSamplerWatcher.py
@@ -16,16 +16,36 @@ def _fake_sampler(stochasticity=1.0, timesteps=10):
     'steps skip type': { 'name': 'uniform', 'K': 1 },
   })
   
-  x = tf.random.normal([32, 3])
-  fakeNoise = tf.random.normal([32, 3])
+  shape = (32, 3)
+  fakeNoise = tf.random.normal(shape)
   def fakeModel(x, T, **kwargs):
     return fakeNoise + tf.cast(T, tf.float32) * x
 
-  return CFakeObject(
-    x=x,
-    model=fakeModel,
-    interpolant=interpolant
-  )
+  x = tf.random.normal(shape)
+  return CFakeObject(x=x, model=fakeModel, interpolant=interpolant)
+
+def _fake_AR(threshold, timesteps=10):
+  interpolant = sampler_from_config({
+    "name": "autoregressive",
+    "noise provider": "normal",
+    "threshold": threshold,
+    "steps": {
+      "start": 1.0,
+      "end": 0.001,
+      "steps": timesteps,
+      "decay": 0.9
+    },
+    "interpolant": { "name": "direction" }
+  })
+ 
+  shape = (32, 3)
+  fakeNoise = tf.random.normal(shape)
+  def fakeModel(x, t, mask, **kwargs):
+    s = tf.boolean_mask(fakeNoise, mask)
+    return s + tf.cast(t, tf.float32) * x
+
+  x = tf.random.normal(shape)
+  return CFakeObject(x=x, model=fakeModel, interpolant=interpolant)
 
 def test_notAffectResults():
   fake = _fake_sampler()
@@ -60,8 +80,8 @@ def test_collectsSteps():
     'Tracked steps must be different from initial value'
   )
   _commonChecks(collectedSteps, fake.x, results)
-  tf.debugging.assert_equal(tf.shape(collectedSteps)[0], 11, 'Must collect 11 steps')
-  tf.debugging.assert_equal(watcher.iteration, 11, 'Must collect 11 steps')
+  tf.debugging.assert_equal(tf.shape(collectedSteps)[0], 11, 'Must collect 11 values')
+  tf.debugging.assert_equal(watcher.iteration, 10, 'Must collect 10 steps')
   return
 
 def test_collectsOnlyIndicedValues():
@@ -77,37 +97,60 @@ def test_collectsOnlyIndicedValues():
   results = fake.interpolant.sample(value=fake.x, model=fake.model, algorithmInterceptor=watcher.interceptor())
 
   collectedSteps = watcher.tracked('value')
-  _commonChecks(collectedSteps, x=tf.gather(fake.x, indices, axis=0), results=tf.gather(results, indices, axis=0))
-  tf.debugging.assert_equal(tf.shape(collectedSteps)[0], 11, 'Must collect 11 steps')
+  _commonChecks(
+    collectedSteps, 
+    x=tf.gather(fake.x, indices, axis=0),
+    results=tf.gather(results, indices, axis=0)
+  )
+  tf.debugging.assert_equal(tf.shape(collectedSteps)[0], 11, 'Must collect 11 values')
   return
 
 def test_resetIteration():
   watcher = CSamplerWatcher(steps=10, tracked=dict())
   fake = _fake_sampler(timesteps=10)
   _ = fake.interpolant.sample(value=fake.x, model=fake.model, algorithmInterceptor=watcher.interceptor())
-  tf.debugging.assert_equal(watcher.iteration, 11, 'Must collect 11 steps')
+  tf.debugging.assert_equal(watcher.iteration, 10, 'Must collect 10 steps')
   
   fake = _fake_sampler(timesteps=5)
   _ = fake.interpolant.sample(value=fake.x, model=fake.model, algorithmInterceptor=watcher.interceptor())
-  tf.debugging.assert_equal(watcher.iteration, 6, 'Must collect 6 steps')
+  tf.debugging.assert_equal(watcher.iteration, 5, 'Must collect 5 steps')
   return
 
-def test_solveStep():
+def _checkTracked(value, N):
+  assert value is not None, 'Must be tracked'
+  tf.debugging.assert_equal(tf.shape(value), (N, 32, 3), 'Unexpected shape')
+  tf.debugging.assert_greater(tf.reduce_sum(tf.abs(value[0] - value[1])), 0.0, 'Must be different')
+  return
+
+def test_trackSolution():
   fake = _fake_sampler()
   watcher = CSamplerWatcher(
     steps=10,
-    tracked=dict(
-      x0=(32, 3),
-      x1=(32, 3),
-    )
+    tracked=dict(x0=(32, 3), x1=(32, 3), value=(32, 3))
   )
   _ = fake.interpolant.sample(value=fake.x, model=fake.model, algorithmInterceptor=watcher.interceptor())
+  _checkTracked(watcher.tracked('x0'), N=10)
+  _checkTracked(watcher.tracked('x1'), N=10)
+  _checkTracked(watcher.tracked('value'), N=11)
+  return
 
-  def check(value):
-    assert value is not None, 'Must be tracked'
-    tf.debugging.assert_equal(tf.shape(value), (11, 32, 3), 'Unexpected shape')
-    tf.debugging.assert_greater(tf.reduce_sum(tf.abs(value[0] - value[1])), 0.0, 'Must be different')
-    return
-  check(watcher.tracked('x0'))
-  check(watcher.tracked('x1'))
+def test_trackSolutionWithMask():
+  fake = _fake_AR(threshold=0.1)
+  watcher = CSamplerWatcher(
+    steps=10,
+    tracked=dict(x0=(32, 3), x1=(32, 3))
+  )
+  _ = fake.interpolant.sample(value=fake.x, model=fake.model, algorithmInterceptor=watcher.interceptor())
+  _checkTracked(watcher.tracked('x0'), N=10)
+  _checkTracked(watcher.tracked('x1'), N=10)
+  return
+
+def test_trackSolutionWithMask_value():
+  fake = _fake_AR(threshold=0.1)
+  watcher = CSamplerWatcher(
+    steps=10,
+    tracked=dict(value=(32, 3))
+  )
+  _ = fake.interpolant.sample(value=fake.x, model=fake.model, algorithmInterceptor=watcher.interceptor())
+  _checkTracked(watcher.tracked('value'), N=11)
   return